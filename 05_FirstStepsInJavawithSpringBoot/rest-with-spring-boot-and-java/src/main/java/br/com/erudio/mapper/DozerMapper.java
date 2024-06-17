a01a032bdfdde01b6c0e5cd15ce8294554bd5aad
caiodassumpcao <caio.rodri.d@gmail.com>
2024-06-17 15:12:11 -0300

    Pradonizin a project VO - add unit test and Model Mapper

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/mapper/DozerMapper.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/mapper/DozerMapper.java
@@ -3,24 +3,26 @@ package br.com.erudio.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
 
-import br.com.erudio.data.vo.v1.PersonVO;
-import br.com.erudio.model.Person;
+//import com.github.dozermapper.core.DozerBeanMapperBuilder;
+//import com.github.dozermapper.core.Mapper;
 
 public class DozerMapper {
-	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	//private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	private static ModelMapper mapper = new ModelMapper();
 	
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
 	}
-	public static <O, D> List<D> parseObject(List<O> origin, Class<D> destination) {
+	
+	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
 		List<D> destinationObjects = new ArrayList<D>();
 		for (O o : origin) {
 			destinationObjects.add(mapper.map(o, destination));
 		}
 		return destinationObjects;
 	}
-	}
+
 }
--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/erudio/unittests/mapper/DozerConverterTest.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/erudio/unittests/mapper/DozerConverterTest.java
@@ -97,4 +97,3 @@ public class DozerConverterTest {
         assertEquals("Male", outputTwelve.getGender());
     }
 }
-
