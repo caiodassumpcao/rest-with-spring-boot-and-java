365ce9c6f4daec8971cfbbb61cf005e875bbdfb1
caiodassumpcao <caio.rodri.d@gmail.com>
2024-07-24 10:14:08 -0300

    Addind a support a migrations whith flyway

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Recife, Pernambuco', 'Caio', 'Male', 'Rodrigues'),
+	(2, 'Praia piedade', 'Jack', 'Male', 'Daniel\'s'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(9, 'Rio de Janiero, Rio de Janiero', 'Arlindo', 'Male', 'Cruz'),
+	(10, 'Smiljan, Croatia', 'Nikola', 'Male', 'Tesla');
+
